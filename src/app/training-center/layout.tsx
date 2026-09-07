24fa9a35ab1f32560ce026255ecdea86d1678b97
Dmoore3393 <youngmoore19@gmail.com>
2026-09-06 18:00:43 -0700

    Apply larger Training Center typography

---
+++ src/app/training-center/layout.tsx
@@ -0,0 +1,5 @@
+import "./readable.css";
+
+export default function TrainingCenterLayout({ children }: { children: React.ReactNode }) {
+  return <div className="training-center-readable">{children}</div>;
+}
